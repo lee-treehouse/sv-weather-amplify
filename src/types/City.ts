1f21ec0ad91274dc8f5cfab670cbb1944b7922a0
lee-treehouse <leentaylor@gmail.com>
2022-03-14 16:24:31 +1100

    adding more cities for kate

--- src/types/City.ts
+++ src/types/City.ts
@@ -7,4 +7,8 @@ export enum City {
   Perth = "2063523",
   Darwin = "2073124",
   Brisbane = "2174003",
+  "San Francisco" = "5391959",
+  "New York City" = "5128581",
+  "Paris" = "2988507",
+  "Moscow" = "524901",
 }
